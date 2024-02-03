f33e0543d889619f8e68ef8f3a265c96795b4891
Rafael <rafaelmarques.rm86@gmail.com>
2024-02-03 20:55:10 -0300

    Criação do modelo de negocio

---
+++ LojaVirtual/LojaVirtual/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿namespace LojaVirtual.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThubnailUrl { get; set; }
+        public bool IsProdutoPreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CatedoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
